f162db9523949309a1478c004e378127a9896aca
efernandes16 <ericafernandesrj@yahoo.com.br>
2021-02-22 17:17:41 -0300

    Adicionando Projeto 'Bons Drinks' ao repositorio

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Navbar from './Componentes/Navbar';
+import HomePage from './Componentes/HomePage';
+import Formulario from './Componentes/Formulario';
+import Footer from './Componentes/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <HomePage />
+      <Formulario />
+      <Footer />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componentes/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <nav>AQUI É A NavBar</nav>
+            </div>
+        )
+    }
+}
